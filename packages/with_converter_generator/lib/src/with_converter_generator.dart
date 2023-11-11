f249c445d23a315bb01fe1329884730ed5018384
htsuruo <tsuru.dev@gmail.com>
2023-11-11 21:30:56 +0900

    Set common analysis_options.yaml on root

--- packages/with_converter_generator/lib/src/with_converter_generator.dart
+++ packages/with_converter_generator/lib/src/with_converter_generator.dart
@@ -4,8 +4,11 @@ import 'package:source_gen/source_gen.dart';
 
 class WithConverterGenerator extends GeneratorForAnnotation<String> {
   @override
-  generateForAnnotatedElement(
-      Element element, ConstantReader annotation, BuildStep buildStep) {
+  String generateForAnnotatedElement(
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  ) {
     return 'foo';
   }
 }
