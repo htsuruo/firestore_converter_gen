941c0572f9c20dbec339f1ff876e5f038973b832
htsuruo <tsuru.dev@gmail.com>
2023-11-12 17:43:02 +0900

    Change annotation directory structure

---
+++ packages/with_converter_annotation/lib/src/annotation.dart
@@ -0,0 +1,3 @@
+class WithConverter {
+  const WithConverter();
+}
--- packages/with_converter_annotation/lib/with_converter_annotation.dart
+++ packages/with_converter_annotation/lib/with_converter_annotation.dart
@@ -1,3 +1,9 @@
-class WithConverter {
-  const WithConverter();
-}
+/// Provides annotation classes to use with
+/// [with_converter_generator](https://pub.dev/packages/json_serializable).
+library with_converter_annotation;
+
+// TODO(htsuruo): 本来は利用元でimportせずに済むようにexportしておきたいが
+// build_runner実行時エラーが出るためコメントアウトしている。
+// export 'package:cloud_firestore/cloud_firestore.dart';
+
+export 'src/annotation.dart';
